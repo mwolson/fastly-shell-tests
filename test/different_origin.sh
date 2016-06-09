92b168bfffc54cb488c94b0c19338c498cc98b50
Michael Olson <mwolson@member.fsf.org>
2016-06-09 16:16:31 -0700

    Message wording

--- test/different_origin.sh
+++ test/different_origin.sh
@@ -13,7 +13,7 @@ record_curl "${target_url}/host/3F004EAECFB7BA10/facets?q=and(and(has(inventoryt
 expect_header X-Cache; to_equal MISS
 expect_header Age; to_equal 0
 
-it "cache hits on a second request with different Origin header, expecting Age of 5 in response"
+it "cache hits on a second request with different Origin header"
 
 wait_on_fastly_cache
 record_curl "${target_url}/host/3F004EAECFB7BA10/facets?q=and(and(has(inventorytypes,%22primary%22),has(offertypes,%22standard%22)),available)&show=facerange&by=accessibility+shape+inventorytypes+offers+offerTypes" -H 'Pragma: no-cache' -H 'Origin: http://fastlyy-hackathon.tmdev.co' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H 'Referer: http://fastly-hackathon.tmdev.co/event/3F004EAECFB7BA10?fg=ism' -H 'Connection: keep-alive' -H 'X-Proxy-Authorization: Basic YjQ2Mm9pN2ZpYzZwZWhjZGt6b255NWJ4aGU6cHF1enBmcmZ6N3pkMnlsdnR6M3c1ZHR5c2U=' --compressed
