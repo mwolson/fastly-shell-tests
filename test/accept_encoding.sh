b260a7770c137af829414500c554c00376ebae26
Michael Olson <mwolson@member.fsf.org>
2016-06-14 15:29:23 -0700

    Check Content-Encoding instead of Accept-Encoding, drop deflate test
    Also check for Set-Cookie header before running tests for it, since it's now untestable in QA.

--- test/accept_encoding.sh
+++ test/accept_encoding.sh
@@ -4,14 +4,14 @@
 
 describe "Accept-Encoding header should be normalized, and it should be used in the cache key"
 
-keep_headers "Age|X-Cache|X-Served-By|accept-encoding"
+keep_headers "Age|X-Cache|X-Served-By|Accept-Encoding|Content-Encoding"
 
 it "misses on the first request"
 
 until_fresh_curl_object 5 record_curl "${target_url}/host/${target_event_id}/facets?q=and(and(has(inventorytypes,%22primary%22),has(offertypes,%22standard%22)),available)&show=facerange&by=accessibility+shape+inventorytypes+offers+offerTypes" -H 'Pragma: no-cache' -H 'Origin: http://fastly-hackathon.tmdev.co' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H "Referer: http://fastly-hackathon.tmdev.co/event/${target_event_id}?fg=ism" -H 'Connection: keep-alive' -H "X-Proxy-Authorization: Basic ${target_proxy_auth_token}" --compressed
 
 expect_header X-Cache; to_match MISS$
-expect_header Accept-Encoding; to_equal gzip
+expect_header Content-Encoding; to_equal gzip
 miss_age=$(get_header Age)
 
 it "cache hits on a request with a simpler encoding list"
@@ -20,24 +20,7 @@ record_curl "${target_url}/host/${target_event_id}/facets?q=and(and(has(inventor
 
 expect_header X-Cache; to_match HIT$
 expect_header Age; to_be_between $((miss_age)) $((miss_age + 2))
-expect_header Accept-Encoding; to_equal gzip
-
-it "checks whether Akamai is present to interfere with the Accept-Encoding header"
-
-record_curl -k "${origin_url}/host/${target_event_id}/facets?q=and(and(has(inventorytypes,%22primary%22),has(offertypes,%22standard%22)),available)&show=facerange&by=accessibility+shape+inventorytypes+offers+offerTypes" -H 'Pragma: no-cache' -H 'Origin: http://fastly-hackathon.tmdev.co' -H 'Accept-Encoding: deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H "Referer: http://fastly-hackathon.tmdev.co/event/${target_event_id}?fg=ism" -H 'Connection: keep-alive' -H "X-Proxy-Authorization: Basic ${target_proxy_auth_token}" --compressed
-
-if test -n "$(get_header Akamai-Origin-Hop)"; then
-    it "detected Akamai, skipping remaining tests"
-    exit 0
-fi
-
-it "cache misses on a request with an Accept-Encoding header of deflate but normalizes it"
-
-record_curl "${target_url}/host/${target_event_id}/facets?q=and(and(has(inventorytypes,%22primary%22),has(offertypes,%22standard%22)),available)&show=facerange&by=accessibility+shape+inventorytypes+offers+offerTypes" -H 'Pragma: no-cache' -H 'Origin: http://fastly-hackathon.tmdev.co' -H 'Accept-Encoding: deflate;q=1.0' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H "Referer: http://fastly-hackathon.tmdev.co/event/${target_event_id}?fg=ism" -H 'Connection: keep-alive' -H "X-Proxy-Authorization: Basic ${target_proxy_auth_token}" --compressed
-
-expect_header X-Cache; to_match MISS$
-expect_header Age; to_be_between $((miss_age)) $((miss_age + 5))
-expect_header Accept-Encoding; to_equal deflate
+expect_header Content-Encoding; to_equal gzip
 
 it "cache misses on a request without an Accept-Encoding header"
 
@@ -45,4 +28,4 @@ record_curl "${target_url}/host/${target_event_id}/facets?q=and(and(has(inventor
 
 expect_header X-Cache; to_match MISS$
 expect_header Age; to_be_between $((miss_age)) $((miss_age + 6))
-expect_header Accept-Encoding; to_be_empty
+expect_header Content-Encoding; to_be_empty
--- test/remove_cookies.sh
+++ test/remove_cookies.sh
@@ -4,13 +4,19 @@
 
 describe "Cookies should be removed from the response by fastly to improve caching"
 
-keep_headers "Age|X-Cache|X-Served-By|Set-Cookie|Cookie"
+keep_headers "Age|X-Cache|X-Served-By|Server|Set-Cookie|Cookie"
 
 it "echoes Set-Cookie on origin server"
 
 until_fresh_curl_object 5 record_curl -k "${origin_url}/host/${target_event_id}/facets?q=and(and(has(inventorytypes,%22primary%22),has(offertypes,%22standard%22)),available)&show=facerange&by=accessibility+shape+inventorytypes+offers+offerTypes" -H 'Pragma: no-cache' -H 'Origin: http://fastly-hackathon.tmdev.co' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H "Referer: http://fastly-hackathon.tmdev.co/event/${target_event_id}?fg=ism" -H 'Connection: keep-alive' -H "X-Proxy-Authorization: Basic ${target_proxy_auth_token}" -H 'Set-Cookie: CMPS=Q5oQ2d5Dp4NJiGw9hOPvVwTUgwpc0LG7oFTYDCL9qFGv7yZOOnpbJuPC1zFXCC450KX5ygu/fEU=; path=/' --compressed
 
 expect_header Server; to_equal nginx
+
+if test -z "$(get_header Set-Cookie)"; then
+    it "skips remaining tests since this origin server doesn't set cookies"
+    exit 0
+fi
+
 expect_header Set-Cookie; to_contain "CMPS="
 miss_age=$(get_header Age)
 
