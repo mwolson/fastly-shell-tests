a018daa0d447f8f0935b82516a177dca6163d5c4
Michael Olson <mwolson@member.fsf.org>
2016-06-20 15:07:21 -0700

    Extend allowed time of origin hop from 5ms to 10ms in cache hit case

--- test/cacheable_urls.sh
+++ test/cacheable_urls.sh
@@ -6,6 +6,8 @@ describe "Only /facets and /quickpicks should be cached by fastly"
 
 keep_headers "Age|X-Cache|X-Served-By|X-Timer|Access-Control-Allow-Origin"
 
+expected_fastly_origin_time=10
+
 function record_facets() {
     record_curl "${target_url}/host/${target_event_id}/facets?by=inventorytypes%20offertypes%20accessibility&q=available&apikey=${target_apikey}&apisecret=${target_apisecret}" -H 'Pragma: no-cache' -H 'Origin: http://fastly-hackathon.tmdev.co' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36' -H 'Accept: */*' -H "Referer: http://fastly-hackathon.tmdev.co/event/${target_event_id}?fg=ism" -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed
 }
@@ -25,7 +27,7 @@ function expect_modified_response() {
 
 function expect_unmodified_response() {
     expect_header Vary; to_equal "Accept, Accept-Language, Accept-Encoding, Origin, TMPS-IdentityToken, X-Api-Key"
-    expect_origin_response_time; to_be_greater_than 5
+    expect_origin_response_time; to_be_greater_than $expected_fastly_origin_time
 }
 
 it "misses on the first facets request"
@@ -47,7 +49,7 @@ record_facets
 expect_header X-Cache; to_match HIT$
 expect_header Age; to_be_between $((miss_age)) $((miss_age + 2))
 expect_modified_response
-expect_origin_response_time; to_be_less_than 5
+expect_origin_response_time; to_be_less_than $expected_fastly_origin_time
 
 it "misses on the first quickpicks request"
 
@@ -64,7 +66,7 @@ record_quickpicks
 expect_header X-Cache; to_match HIT$
 expect_header Age; to_be_between $((miss_age)) $((miss_age + 2))
 expect_modified_response
-expect_origin_response_time; to_be_less_than 5
+expect_origin_response_time; to_be_less_than $expected_fastly_origin_time
 
 it "misses on the first offers request"
 
